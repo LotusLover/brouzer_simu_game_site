4c0d695266bf4dcfd63252ffc6cc6e4c00ce97f4
LotusLover <108057253+LotusLover@users.noreply.github.com>
2026-01-20 17:27:48 +0900

    enemy_kill_fix

--- docs/Build/docs.framework.js
+++ docs/Build/docs.framework.js
@@ -2085,13 +2085,13 @@ var tempI64;
 // === Body ===
 
 var ASM_CONSTS = {
-  5283728: function() {return Module.webglContextAttributes.premultipliedAlpha;},  
- 5283789: function() {return Module.webglContextAttributes.preserveDrawingBuffer;},  
- 5283853: function() {return Module.webglContextAttributes.powerPreference;},  
- 5283911: function() {Module['emscripten_get_now_backup'] = performance.now;},  
- 5283966: function($0) {performance.now = function() { return $0; };},  
- 5284014: function($0) {performance.now = function() { return $0; };},  
- 5284062: function() {performance.now = Module['emscripten_get_now_backup'];}
+  5283760: function() {return Module.webglContextAttributes.premultipliedAlpha;},  
+ 5283821: function() {return Module.webglContextAttributes.preserveDrawingBuffer;},  
+ 5283885: function() {return Module.webglContextAttributes.powerPreference;},  
+ 5283943: function() {Module['emscripten_get_now_backup'] = performance.now;},  
+ 5283998: function($0) {performance.now = function() { return $0; };},  
+ 5284046: function($0) {performance.now = function() { return $0; };},  
+ 5284094: function() {performance.now = Module['emscripten_get_now_backup'];}
 };
 
 
